6e6b17cc18f55c98fef854a91dcd10d216ea93b1
Chenna Kautilya <chenna@outlook.com>
2025-05-02 15:08:41 -0700

    ci: use system gcc

--- development/scripts/pixi/activation.sh
+++ development/scripts/pixi/activation.sh
@@ -19,7 +19,7 @@ elif [[ $host_alias == *"linux"* ]];
 then
   # On GNU/Linux, I don't know if these flags are mandatory with g++ but
   # it allow to use clang++ as compiler
-  export LDFLAGS="-Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath-link,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
+  # export LDFLAGS="-Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath-link,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
 
   # Conda compiler is named x86_64-conda-linux-gnu-c++, ccache can't resolve it
   # (https://ccache.dev/manual/latest.html#config_compiler_type)
